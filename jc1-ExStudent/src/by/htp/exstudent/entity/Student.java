bfb4a42510266045d6446dd2af7f9371a33ff8d4
iMateshuk <80713398+iMateshuk@users.noreply.github.com>
2021-04-17 21:45:59 +0300

    Add files via upload

---
+++ jc1-ExStudent/src/by/htp/exstudent/entity/Student.java
@@ -0,0 +1,84 @@
+package by.htp.exstudent.entity;
+
+import java.util.Arrays;
+
+public class Student {
+
+	private String fio;
+	private int groupNumber;
+	private int[] progress;
+
+	public Student(String fio, int groupNumber) {
+		this.fio = fio;
+		this.groupNumber = groupNumber;
+	}
+	
+	public Student(String fio, int groupNumber, int[] progress) {
+		this.fio = fio;
+		this.groupNumber = groupNumber;
+		this.progress = progress;
+	}
+
+	public void setFio(String fio) {
+		this.fio = fio;
+	}
+
+	public String getFio() {
+		return fio;
+	}
+
+	public void setGroupNumber(int groupNumber) {
+		this.groupNumber = groupNumber;
+	}
+
+	public int getGroupNumber() {
+		return groupNumber;
+	}
+
+	public void setProgress(int[] progress) {
+		this.progress = progress;
+	}
+
+	public int[] getProgress() {
+		return progress;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fio == null) ? 0 : fio.hashCode());
+		result = prime * result + groupNumber;
+		result = prime * result + Arrays.hashCode(progress);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (fio == null) {
+			if (other.fio != null)
+				return false;
+		} else if (!fio.equals(other.fio))
+			return false;
+		if (groupNumber != other.groupNumber)
+			return false;
+		if (!Arrays.equals(progress, other.progress))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [fio=" + fio + ", groupNumber=" + groupNumber + ", progress=" + Arrays.toString(progress) + "]";
+	}
+	
+	
+
+}
